521abb7f4f467a23405e86792821fdf59c06beb4
BillFukngMurray <t.banaszewski@ostfalia.de>
2022-08-23 16:59:41 +0200

    Added and completed the reverseString  excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function (word) {
+
+    const arrayString = word.split("");
+    const reversedArray = [];
+    let concatString = "";
+    // console.log(arrayString.toString());
+
+    for (let i = 0; i < arrayString.length; i++) {
+
+        reversedArray[i] = arrayString[(arrayString.length - 1) - i];
+
+    }
+    concatString = reversedArray.join("");
+
+    // console.log(concatString);
+
+    return concatString;
 
 };
+
+//Oder einfach: return string.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
