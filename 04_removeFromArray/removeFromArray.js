8d9b18c894bde0a1a1876db8e1067511a4c7322a
BillFukngMurray <t.banaszewski@ostfalia.de>
2022-08-24 11:09:48 +0200

    Haven't solved this one yet

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === args){
+            array.splice(i, 1);
+            continue;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
