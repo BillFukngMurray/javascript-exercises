9d78d73ae3fd36f45d3e665726e20d79c7aea5d6
BillFukngMurray <t.banaszewski@ostfalia.de>
2022-08-26 14:44:53 +0200

    Added and completed palindromes excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let pali = string.trim().split(" ").join("").replace(/\W/g, '').toLowerCase(); // removes any whitespaces from the string and converts to lowercase.
+    // replace(/\W/g, '') allows only numbers and letters
+
+    for (let i = 0; i < pali.length - 1 / 2; i++) {
+        if (pali.charAt(i) !== pali.charAt(pali.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 
 };
+
+// oder:
+// processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+//   return (
+//     processedString
+//       .split("")
+//       .reverse()
+//       .join("") == processedString
+//   );
+
 
 // Do not edit below this line
 module.exports = palindromes;
