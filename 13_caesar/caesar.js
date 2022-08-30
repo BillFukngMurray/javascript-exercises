ea0db47352a276355cceb821e9fc9e86467aefbb
BillFukngMurray <t.banaszewski@ostfalia.de>
2022-08-30 13:17:44 +0200

    Started caesar excercise

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,14 @@
-const caesar = function() {
+const caesar = function(inputString, shift) {
 
 };
+
+
+
+function shiftCode(char, shift){
+
+    const code = char.charCodeAt();
+
+}
 
 // Do not edit below this line
 module.exports = caesar;
